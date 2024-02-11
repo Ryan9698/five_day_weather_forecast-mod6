7605c48e3e7905468ff1e214e8fbd9da27ada8b2
Ryan Caltabiano <rcaltabiano90@gmail.com>
2024-02-11 16:04:18 -0500

    Added favicon and updated the README. Still needs to be completed. Code is finished and app is functional with no errors.

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# five_day_weather_forecast-mod6
-A page utilizing a five day weather forecast API
+# Five Day Weather Forecast - Module 6 Challenge
+
+## Table of Contents
+
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Contributors](#contributors)
+5. [License](#license)
+6. [Link](#link)
+7. [Screenshot](#screenshot)
+8. [Questions](#questions)
+
+## Description
+
+This web app is designed to notify the user of current weather conditions in the city of their choice. It's meant to be an easily accessible app for determining the upcoming weather. Working through this app, I learned a lot about basic API calls and the data that is returned and how to modify that data to render on a webpage. I decided to add a couple of other features such as the automatic conversion of Kelvin (API return) to Fahrenheit and displaying "Feels like" to make it a more in line with authentic weather applications. Icons are displayed based on weather conditions so you can quickly tell what kind of weather is in the area at a glance.
+
+## Installation
+
+N/A
+
+## Usage
+
+Using the project is easy, type in the city you wish to see the current weather for and click the "Search Weather" button. The app will display the current weather as well as a 5 day weather forecast for the cityof your choice. A button with the city will generate allowing you to refer back to it. The information is saved to local storage. Searches are configured for US cities.
+
+## Contributors
+
+This app uses APIs from https://openweathermap.org/api
+
+Favicon sourced from Twitter Twemoji:
+
+- Graphics Title: 2601.svg
+- Graphics Author: Copyright 2020 Twitter, Inc and other contributors (https://github.com/twitter/twemoji)
+- Graphics Source: https://github.com/twitter/twemoji/blob/master/assets/svg/2601.svg
+- Graphics License: CC-BY 4.0 (https://creativecommons.org/licenses/by/4.0/)
+
+## License
+
+No license for this app.
+
+## Link
+
+[GitHub_Pages]()
+
+## Screenshot
+
+![dashboard](./assets/dashboard.png)
+
+## Questions
+
+If you have any questions or would like to see my work, you can see my GitHub portfolio: [Ryan9698](https://github.com/Ryan9698).
+
+or reach me by email: [rcaltabiano90@gmail.com](mailto:rcaltabiano90@gmail.com).
--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const apiKey = '6d618859ee1bca60237c11fbce3184ca';
+const apiKey = "6d618859ee1bca60237c11fbce3184ca";
 
 // Retrieve search history from local storage or initialize an empty array if not present
-const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+const searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
 function displayCurrentWeather(data, city) {
-  const weatherInfo = document.getElementById('weatherInfo');
+  const weatherInfo = document.getElementById("weatherInfo");
   const weather = data.weather[0];
   const tempFahrenheit = convertKelvinToFahrenheit(data.main.temp);
   const feelsLikeFahrenheit = convertKelvinToFahrenheit(data.main.feels_like);
 
   weatherInfo.innerHTML = `
-    <h2>${city}<img src="http://openweathermap.org/img/wn/${weather.icon}.png" alt="Weather Icon"></h2>
+    <h2>${city}<img src="http://openweathermap.org/img/wn/${
+    weather.icon
+  }.png" alt="Weather Icon"></h2>
     <p>Date: ${getCurrentDate()}</p>
     <p>Temperature: ${tempFahrenheit.toFixed(2)}°F</p>
     <p>Feels Like: ${feelsLikeFahrenheit.toFixed(2)}°F</p>
@@ -21,10 +23,11 @@ function displayCurrentWeather(data, city) {
 }
 // Display 5-day forecast as styled cards with temperatures converted to Fahrenheit
 function displayFiveDayForecast(fiveDayForecast) {
-  const forecastContainer = document.getElementById('forecast');
-  forecastContainer.innerHTML = '<h2 class="mt-4 text-center">5-Day Forecast</h2>';
+  const forecastContainer = document.getElementById("forecast");
+  forecastContainer.innerHTML =
+    '<h2 class="mt-4 text-center">5-Day Forecast</h2>';
 
-  fiveDayForecast.forEach(entry => {
+  fiveDayForecast.forEach((entry) => {
     const date = new Date(entry.dt * 1000);
     const weather = entry.weather[0];
     const tempFahrenheit = convertKelvinToFahrenheit(entry.main.temp);
@@ -36,7 +39,9 @@ function displayFiveDayForecast(fiveDayForecast) {
         <p>Wind: ${entry.wind.speed} mph</p>
         <p>Humidity: ${entry.main.humidity}%</p>
         <p>Weather: ${weather.main}</p>
-        <img src="http://openweathermap.org/img/wn/${weather.icon}.png" alt="Weather Icon">
+        <img src="http://openweathermap.org/img/wn/${
+          weather.icon
+        }.png" alt="Weather Icon">
       </div>
     `;
   });
@@ -48,21 +53,23 @@ function getCurrentDate() {
 }
 
 function convertKelvinToFahrenheit(tempKelvin) {
-  return (tempKelvin - 273.15) * 9 / 5 + 32;
+  return ((tempKelvin - 273.15) * 9) / 5 + 32;
 }
 
 function createCityButton(city) {
-  const cityList = document.getElementById('savedSearches');
-  if (Array.from(cityList.children).some(button => button.textContent === city)) {
-    console.warn('Button for this city already exists.');
+  const cityList = document.getElementById("savedSearches");
+  if (
+    Array.from(cityList.children).some((button) => button.textContent === city)
+  ) {
+    console.warn("Button for this city already exists.");
     return;
   }
 
-  const cityButton = document.createElement('button');
-  cityButton.className = 'btn btn-secondary w-100 my-2';
+  const cityButton = document.createElement("button");
+  cityButton.className = "btn btn-secondary w-100 my-2";
   cityButton.textContent = city;
-  cityButton.type = 'button';
-  cityButton.addEventListener('click', () => {
+  cityButton.type = "button";
+  cityButton.addEventListener("click", () => {
     fetchWeatherData(city);
   });
 
@@ -70,44 +77,55 @@ function createCityButton(city) {
 }
 //Fetch weather data from API and calling displayCurrentWeather and displayFiveDayForecast
 function fetchWeatherData(city) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-    .then(response => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+  )
+    .then((response) => {
       if (!response.ok) {
-        throw new Error('Network response was not ok.');
+        throw new Error("Network response was not ok.");
       }
       return response.json();
     })
-    .then(data => {
+    .then((data) => {
       displayCurrentWeather(data, city);
-      return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`);
+      return fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+      );
     })
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
-        throw new Error('Network response was not ok.');
+        throw new Error("Network response was not ok.");
       }
       return response.json();
     })
-    .then(data => {
-      const fiveDayForecast = data.list.filter(entry => new Date(entry.dt_txt).getHours() === 12);
+    .then((data) => {
+      const fiveDayForecast = data.list.filter(
+        (entry) => new Date(entry.dt_txt).getHours() === 12
+      );
       displayFiveDayForecast(fiveDayForecast);
     })
-    .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
+    .catch((error) => {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
     });
 }
 
-document.getElementById('weatherForm').addEventListener('submit', function (event) {
-  event.preventDefault();
-  const city = document.getElementById('cityInput').value.trim();
-  if (city && !searchHistory.includes(city)) {
-    searchHistory.push(city);
-    localStorage.setItem('searchHistory', JSON.stringify(searchHistory)); // Update local storage
-    createCityButton(city);
-  }
-  fetchWeatherData(city);
-});
+document
+  .getElementById("weatherForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const city = document.getElementById("cityInput").value.trim();
+    if (city && !searchHistory.includes(city)) {
+      searchHistory.push(city);
+      localStorage.setItem("searchHistory", JSON.stringify(searchHistory)); // Update local storage
+      createCityButton(city);
+    }
+    fetchWeatherData(city);
+  });
 
 //City buttons from local storage
-searchHistory.forEach(city => {
+searchHistory.forEach((city) => {
   createCityButton(city);
 });
